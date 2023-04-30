a77bfef7336bf3f4196e2d6d643329768efb8241
Nagy Daoud <nagy.daoud@deliveryhero.com>
2023-04-30 12:19:36 +0200

    Solving to quiz2

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,11 +10,14 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(n: i64) -> i64 {
+    if n <= 40 {
+        n * 2
+    } else {
+        n
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
